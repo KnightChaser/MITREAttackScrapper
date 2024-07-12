0e42cbcae71b4989aac3e9287bfc411122415e79
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2024-07-12 13:38:06 +0900

    Update to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="MITREAttackScrapper",
-    version="0.1.0",
+    version="0.1.1",
     author="KnightChaser",
     author_email="agerio100@naver.com",
     description="A package for conveniently retrieving MITRE ATT&CK data",
